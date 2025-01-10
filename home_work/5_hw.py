eefc50b9d9c03131b00e7a838a7e23a867ca8136
Денис <dundzds@gmail.com>
2025-01-10 15:45:54 +0300

    1

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -6,12 +6,14 @@ driver = webdriver.Firefox()
 driver.get ("https://www.saucedemo.com/")
 
 # поиск элемента
-username_field = driver.find_element(By.CSS_SELECTOR,' #user-name ')
-password_field = driver.find_element(By.CSS_SELECTOR, ' #password ')
-button_submit = driver.find_element(By.CSS_SELECTOR, ' #login-button ')
+def search():
+    username_field = driver.find_element(By.CSS_SELECTOR,' #user-name ')
+    password_field = driver.find_element(By.CSS_SELECTOR, ' #password ')
+    button_submit = driver.find_element(By.CSS_SELECTOR, ' #login-button ')
 
-if (username_field or password_field or button_submit) is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
+    if (username_field or password_field or button_submit) is None:
+        print('Элемент не найден')
+    else:
+        print('Элемент найден')
 
+search()
